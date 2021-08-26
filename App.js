1d81ab2f801d764c52022828a09d4242db31a71b
Masa <katsukatsu.masa.0522@gmail.com>
2021-08-27 02:12:04 +0900

    home UI

--- App.js
+++ App.js
@@ -1,19 +1,55 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {View, Text, Image, ImageBackground, StyleSheet} from 'react-native';
 
-export default function App() {
+const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Hello World</Text>
+      <View style={styles.card}>
+        <ImageBackground
+          source={require('./Images/Masa.png')}
+          style={styles.image}>
+          <View style={styles.cardInner}>
+            <Text style={styles.userName}>Masa 28</Text>
+            <Text style={styles.userIntroduction}>大学生</Text>
+          </View>
+        </ImageBackground>
+      </View>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  card: {
+    width: '95%',
+    height: '75%',
+    backgroundColor: 'red',
+    borderRadius: 10,
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+    borderRadius: 10,
+    overflow: 'hidden',
+    justifyContent: 'flex-end',
+  },
+  cardInner: {
+    padding: 10,
+  },
+  userName: {
+    fontSize: 30,
+    color: 'white',
+    fontWeight: 'bold',
+  },
+  userIntroduction: {
+    fontSize: 20,
+    color: 'white',
+    lineHeight: 30,
+  },
 });
+
+export default App;
